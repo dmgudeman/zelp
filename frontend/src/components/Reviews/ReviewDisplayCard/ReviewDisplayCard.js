fd944d48812aa20b47abb880ff8afac84fb4689d
David M Gudeman <davidmgudeman@gmail.com>
2023-05-16 06:05:24 -0700

    style mini buttons

--- frontend/src/components/Reviews/ReviewDisplayCard/ReviewDisplayCard.js
+++ frontend/src/components/Reviews/ReviewDisplayCard/ReviewDisplayCard.js
@@ -4,7 +4,7 @@ import DisplayRating from "../RatingDisplay/RatingDisplay";
 
 const ReviewDisplayCard = ({ review, deleteHandler, editHandler }) => {
     const { body, photoUrl, rating, businessId } = { ...review };
-    const [truncBody, setTruncBody] = useState(body.substring(0, 150) + "...");
+    const [truncBody, setTruncBody] = useState(body.substring(0, 100) + "...");
     const [cardTotal, setCardTotal] = useState(6);
 
     return (
@@ -12,12 +12,22 @@ const ReviewDisplayCard = ({ review, deleteHandler, editHandler }) => {
             <div id="reviewCardContainer">
                 <div id="bodyContainer">
                     <div id="bodyCell">
-                        <DisplayRating rating={rating} starClass="starCard" />  
+                        <DisplayRating rating={rating} starClass="starCard" />
                         <div id="truncWidth">{truncBody} </div>
-                        <button className="editButton" onClick={()=>editHandler(review.id)}>edit</button>
-                        <button className="delButton" onClick={()=>deleteHandler(review.id)}>del</button>
-                        
-
+                        <div className="miniButtonContainer">
+                            <button
+                                className="editButton miniButton"
+                                onClick={() => editHandler(review.id)}
+                            >
+                                edit
+                            </button>
+                            <button
+                                className="delButton miniButton"
+                                onClick={() => deleteHandler(review.id)}
+                            >
+                                del
+                            </button>
+                        </div>
                     </div>
                 </div>
                 {photoUrl ? (
