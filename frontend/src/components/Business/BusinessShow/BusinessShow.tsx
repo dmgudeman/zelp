afc4b49bb9290bebd58fff4fc01988c3cc220944
David Gudeman <davidmgudeman@gmail.com>
2023-06-13 10:01:07 -0700

    ditto

--- frontend/src/components/Business/BusinessShow/BusinessShow.tsx
+++ frontend/src/components/Business/BusinessShow/BusinessShow.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch as _useDispatch  } from "react-redux";
 import BusinessShowDisplay from "../BusinessShowDisplay/BusinessShowDisplay";
 import ReviewsIndex from "../../Reviews/ReviewsIndex/ReviewsIndex";
 import NavBar from "../../Navigation/NavBar/NavBar";
@@ -8,13 +8,16 @@ import ReviewModal from "../../Modals/ReviewModal/ReviewModal";
 import { getBusiness, fetchBusiness } from "../../../store/businessesSlice";
 import { fetchReviewsByBusiness, getReviews } from "../../../store/reviewsSlice";
 import { getTags } from "../../../store/tagsSlice";
+import type {AppDispatch} from "../../../store/store";
+import type {Review} from "../../../Types/ReviewTypes"
 import "./BusinessShow.css";
+const useDispatch = () => _useDispatch<AppDispatch>();
 
-const BusinessShow = (props) => {
+const BusinessShow = () => {
     const dispatch = useDispatch();
     const { busId } = useParams();
     let business = useSelector(getBusiness(busId));
-    const [localReviews, setLocalReviews] = useState([]);
+    const [localReviews, setLocalReviews] = useState<Review[]>([]);
     let tags = useSelector(getTags);
     useEffect(() => {
         if (busId) {
@@ -25,7 +28,7 @@ const BusinessShow = (props) => {
     }, [dispatch, busId]);
 
     useEffect(() => {
-        if (business?.reviews) {
+        if ( business && business.reviews) {
             setLocalReviews(business.reviews);
         }
     }, [business]);
@@ -36,7 +39,7 @@ const BusinessShow = (props) => {
     return (
         <>
             
-            <NavBar className="navIndexBackground" showFlag={true} />
+            <NavBar showFlag={true} />
             <div id="businessShowContainer">
                 {/* 
                     <div className="businessName">{business.name}</div>
@@ -44,7 +47,7 @@ const BusinessShow = (props) => {
 
                 <div
                     style={{
-                        backgroundImage: `url(${business.imageUrls[0]})`,
+                        backgroundImage: `url(${business.imageUrls?.[0]})`,
                         backgroundRepeat: "no-repeat",
                         backgroundPosition: "center",
                         backgroundSize: "cover",
@@ -71,45 +74,3 @@ const BusinessShow = (props) => {
 
 export default BusinessShow;
 
-// const BannerBackground = ({ businesses }) => {
-//     const imgUrl = businesses[0]?.imageUrls
-//         ? businesses[0].imageUrls[0]
-//         : "https://zelp99-seeds.s3.us-west-1.amazonaws.com/Sprouts_a1.jpeg";
-//     return (
-//         <>
-//             <div
-//                 className="backgroundContainer"
-//                 style={{
-//                     backgroundImage: `url(${imgUrl})`,
-//                     backgroundSize: "cover",
-//                     backgroundPosition: "top",
-//                     opacity: 0.5,
-//                 }}
-//             ></div>
-//         </>
-//     );
-// };
-
-// const Banner = (props) => {
-//     return (
-//         <>
-//             <div className="businessContainer">
-//                 <div className="bannerContainer">
-//                     <div className={"leftBanner"}></div>
-//                     <div className="centerBanner">
-//                         <h2>Find a business to review</h2>
-//                         <p className="secondLine">
-//                             Review anything from your favorite patios spot to
-//                             your local flower shop.
-//                         </p>
-
-//                         <div className="bottomBanner">
-//                             <div>Visited one of these places recently?</div>
-//                         </div>
-//                     </div>
-//                     <div className="rightBanner"></div>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
