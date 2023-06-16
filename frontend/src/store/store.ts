a277e52ebdb98f1eefcda2c5e7b481f649c8ce54
David Gudeman <davidmgudeman@gmail.com>
2023-06-16 09:33:46 -0700

    ditto

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -26,6 +26,8 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== 'production',
 });
 
+
+
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
